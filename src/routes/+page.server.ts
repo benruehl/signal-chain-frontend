396887df467eb64eff353f5957d552e1364863f8
Benjamin Rühl <dev.ruehl@gmail.com>
2024-01-06 18:01:59 +0100

    fix: fix page load type

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
 import { fetchDevices } from '$lib/server';
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
     return {
         devices: await fetchDevices(fetch)
     }
-}) satisfies PageLoad;
+}) satisfies PageServerLoad;
