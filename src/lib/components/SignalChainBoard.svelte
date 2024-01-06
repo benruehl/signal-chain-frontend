38f7de0fbb10ae5051847d2cb62fa368a87a96ce
Benjamin Rühl <dev.ruehl@gmail.com>
2024-01-06 17:17:00 +0100

    feat: add svelvet board

--- src/lib/components/SignalChainBoard.svelte
+++ src/lib/components/SignalChainBoard.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
+    import { Node, Svelvet, Minimap, Controls } from 'svelvet';
     import type { Device } from "$lib/models";
 
     export let devices: Device[];
 </script>
 
 <section class="root">
-    {#each devices as device}
-        <div class="device-container">
-            <h3>{device.title}</h3>
-        </div>
-    {/each}
+    <Svelvet controls locked>
+        {#each devices as device}
+            <Node
+                id="node-{device.id}"
+                label="{device.title}"
+                position={{x: device.positionX, y: device.positionY}}/>
+        {/each}
+    </Svelvet>
 </section>
 
 <style lang="sass">
-
+.root
+    width: 100%
+    height: calc(100vh - 1rem)
 </style>
